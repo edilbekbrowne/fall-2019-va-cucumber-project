1ec5b75ef0e3b8b4f763d8256ac8690195bc9281
Edilbek Browne <edilbek.browne@gmail.com>
2020-04-26 12:00:06 -0400

    My first commit

--- src/test/java/com/cybertek/library/utilities/LibraryConstants.java
+++ src/test/java/com/cybertek/library/utilities/LibraryConstants.java
@@ -1,6 +1,7 @@
 package com.cybertek.library.utilities;
 
 public class LibraryConstants {
+    // we made it smaller case, and in LoginStepDefs, we make user.tolowercase-> so they match
     public static final String STUDENT = "student";
     public static final String LIBRARIAN = "librarian";
 }
